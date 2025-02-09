112db7beb7d3388e11c020f5226fa827a90f962e
Floressek <szymon.florek@student.wat.edu.pl>
2025-02-09 11:02:40 +0100

    Add comment for user agent string in listings-scraper.js IF DEMO FAILS

--- src/scrapers/puppeteer/listings-scraper.js
+++ src/scrapers/puppeteer/listings-scraper.js
@@ -68,6 +68,7 @@ class PuppeteerListingsScraper extends BaseScraper {
             'Connection': 'keep-alive',
             'Cache-Control': 'max-age=0'
         });
+        // const userAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36');
         const userAgent = getRandomUserAgent();
         await page.setUserAgent(userAgent);
     }
